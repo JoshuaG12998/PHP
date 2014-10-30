b2beb3ab2f0aa028023a9672f36cf122d264d441
Joshua Garcia <j.garcia12998@gmail.com>
2014-10-30 08:48:30 -0700

    3rd commit

--- Controller/create-db.php
+++ Controller/create-db.php
@@ -2,8 +2,10 @@
 	//Directory of File. return of an include
 	require_once(__DIR__ . "/../model/database.php");
 
+		//server (name of host), username (user that uses server process), password (empty password)
 	$connection = new mysqli($host, $username, $password);
 
+		//Checks if connection has a connection error
 	if($connection->connect_error){
 		die("Error: " . $connection->connection_error);
 	}
@@ -22,7 +24,19 @@
 }
 //Displays since database already exists
 	else {
-		echo "Database already exists";
+		echo "Database already exists.";
 }
+
+
+//stores a query
+			//creates ech post to have an id, a title, text, and a key
+	$query = $connection->query("CREATE TABLE posts ("
+			// creates ids for posts
+		 . "id int(11) NOT NULL AUTO_INCREMENT,"
+		 . "title varchar(255) NOT NULL,"
+			// creates post colum
+		 . "post text NOT NULL,"
+		 	//sets key to id
+		 . "PRIMARY KEY (id)");
 
 	$connection->close();
