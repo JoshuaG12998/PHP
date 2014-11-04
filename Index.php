0159f91433cf4257b1717c33216a10aa3df0eba8
Joshua Garcia <j.garcia12998@gmail.com>
2014-11-04 08:42:31 -0800

    6th commit

--- Index.php
+++ Index.php
@@ -2,6 +2,8 @@
 	<?php
 			//insert header code here
 		require_once(__DIR__ . "/view/Header.php");
+			//Links navigation
+		require_once(__DIR__ . "/view/Navigation.php");
 		require_once(__DIR__ . "/Controller/create-db.php");
 			//insert footer code here
 		require_once(__DIR__ . "/view/Footer.php");
---
+++ View/Navigation.php
@@ -0,0 +1,5 @@
+<nav>
+	<ul>
+		<li><a href="../Post.php">Blog Post Form</a></li>
+	</ul>
+</nav>
