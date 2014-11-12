e5737fbd1fa44673fca56a359ea83ed836b83e5c
Joshua Garcia <j.garcia12998@gmail.com>
2014-11-12 08:41:19 -0800

    12 commit

--- Controller/create-post.php
+++ Controller/create-post.php
@@ -2,6 +2,11 @@
 
 	//saves information from submit button
 
+	//access' variables in database.php
+	require_once(__DIR__ . "/../model/database.php");
+
+	//opens connection
+	$connection = new mysqli($host, $username, $password, $database);
 	//filters input so no one can hack                   //sanitizes illegal characters
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
@@ -9,3 +14,6 @@
 		//Echos variables that was posted
 	echo "<p>Title: $title</p>";
 	echo "<p>Post: post</p>";
+
+	//close connection
+	$connection->close();
