3f8fc784264fa4efc5ab9bcdd78989bc1a441f91
Joshua Garcia <j.garcia12998@gmail.com>
2014-11-13 08:52:28 -0800

    13th commit

--- Controller/create-post.php
+++ Controller/create-post.php
@@ -1,3 +1,18 @@
+<html>
+<head>
+<title></title>
+ <link rel="stylesheet" media="screen and (min-width: 0px)" type="text/css" href="CSS/ipad.css">
+<meta name="viewport" content="width=device-width">
+<meta name="viewport" content='width=768'>
+<style type="text/css"></style>
+</head>
+<body>
+
+
+
+
+
+
 <?php
 
 	//saves information from submit button
@@ -11,9 +26,25 @@
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
-		//Echos variables that was posted
-	echo "<p>Title: $title</p>";
-	echo "<p>Post: post</p>";
+		//true or not true. True will be stores in querry. False will not
+	$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
+
+	//true or false for querry
+if($query) {
+	echo "<p>Successfully inserted post: $title</p>";	
+}
+else {
+	echo "<p>$connection->error</p>";
+}
+
+
 
 	//close connection
 	$connection->close();
+?>
+
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+<script type="text/javascript" href="js/bootstrap.min.js"></script>
+	</body>
+	</html>
