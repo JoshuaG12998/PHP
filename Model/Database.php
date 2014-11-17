9ce8d0d85a398ce78c3cd318a33351466077a549
Joshua Garcia <j.garcia12998@gmail.com>
2014-11-17 10:21:53 -0800

    16th commit

--- Model/Database.php
+++ Model/Database.php
@@ -1,10 +1,19 @@
 <?php
-//personal object
+// create personal object
 class Database {
-	// can only be accessed in this class
+	// private can only be accessed in this class or a class
+	//database holds our variables
 	private $connection;
 	private $host;
 	private $username;
 	private $password;
 	private $database;
+
+	//creates a new cinstructer
+	public function __construct($host, $username, $password, $database) {
+		$this->host = $host; 
+		$this->username = $username;
+		$this->password = $password;
+		$this->database = $database;
+	}
 }
