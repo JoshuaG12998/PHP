5653313c0ca136fe4291d91ab77e86456b800a33
Joshua Garcia <j.garcia12998@gmail.com>
2014-12-02 08:58:47 -0800

    25th commit

---
+++ View/register-form.php
@@ -0,0 +1,22 @@
+	<!-- Creates a register page -->
+	<form>
+		<div>
+		<label for-"email">Email:</label>
+		<input type="text" name="email" /> 
+		</div>
+	</form>
+
+	<div>
+	<label for="username">Username: </label>
+	<input type="text" name="username" />
+	</div>
+
+	<div>
+	<label for="password">Password: </label>
+	<input type="password" name="password" />
+	</div>
+
+	<div>
+	<button type="submit">Submit</button>
+	</div>
+</form>
