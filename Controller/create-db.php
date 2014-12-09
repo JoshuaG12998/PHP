8ac22e6659cfe871a33f4e6b787a1f4e468d0466
Joshua Garcia <j.garcia12998@gmail.com>
2014-12-09 08:35:17 -0800

    27th commit

--- Controller/create-db.php
+++ Controller/create-db.php
@@ -23,7 +23,7 @@
 	}
 
 	
-$query = $_SESSION["$connection"]->query("CREATE TABLE users ("
+$query = $_SESSION["connection"]->query("CREATE TABLE users ("
 	. "id int(11) NOT NULL AUTO_INCREMENT,"
 	. "username varchar(30) NOT NULL,"
 	. "email varchar(50) NOT NULL," 
--- View/register-form.php
+++ View/register-form.php
@@ -1,22 +1,30 @@
 	<!-- Creates a register page -->
-	<form>
+	<?php
+		require_once(__DIR__ . "/../Model/Config.php");
+	?>
+
+
+
+
+	<h1>Register</h1>
+
+	<form method="post" action="<?php echo $path . "Controller/create-user.php"; ?>">
 		<div>
 		<label for-"email">Email:</label>
 		<input type="text" name="email" /> 
 		</div>
+
+		<div>
+		<label for="username">Username: </label>
+		<input type="text" name="username" />
+		</div>
+
+		<div>
+		<label for="password">Password: </label>
+		<input type="password" name="password" />
+		</div>
+
+		<div>
+		<button type="submit">Submit</button>
+		</div>
 	</form>
-
-	<div>
-	<label for="username">Username: </label>
-	<input type="text" name="username" />
-	</div>
-
-	<div>
-	<label for="password">Password: </label>
-	<input type="password" name="password" />
-	</div>
-
-	<div>
-	<button type="submit">Submit</button>
-	</div>
-</form>
